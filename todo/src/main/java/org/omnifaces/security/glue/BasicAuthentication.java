691a4a768b2abd17c22b6fcc1f3e20b3700f2b22
Adam Bien <abien@adam-bien.com>
2015-04-19 10:17:30 +0200

    Startup initialization parameterized with @Alternative @Stereotypes

---
+++ todo/src/main/java/org/omnifaces/security/glue/BasicAuthentication.java
@@ -0,0 +1,22 @@
+package org.omnifaces.security.glue;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Stereotype;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Alternative
+@Stereotype
+@Retention(RUNTIME)
+@Target({METHOD, FIELD, PARAMETER, TYPE})
+public @interface BasicAuthentication {
+}
--- todo/src/main/java/org/omnifaces/security/jaspic/authmodules/BasicAuthModule.java
+++ todo/src/main/java/org/omnifaces/security/jaspic/authmodules/BasicAuthModule.java
@@ -12,66 +12,67 @@
  */
 package org.omnifaces.security.jaspic.authmodules;
 
-import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
-import static org.omnifaces.security.cdi.Beans.getReferenceOrNull;
-import static org.omnifaces.security.jaspic.Utils.isEmpty;
-
+import org.omnifaces.security.glue.BasicAuthentication;
 import javax.security.auth.message.AuthException;
 import javax.security.auth.message.AuthStatus;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
+import static org.omnifaces.security.cdi.Beans.getReferenceOrNull;
+import static org.omnifaces.security.jaspic.Utils.isEmpty;
 import org.omnifaces.security.jaspic.core.HttpMsgContext;
 import org.omnifaces.security.jaspic.core.HttpServerAuthModule;
 import org.omnifaces.security.jaspic.user.UsernamePasswordIdentityStore;
 
 /**
  * Authentication module that authenticates using basic authentication
- * 
+ *
  * <p>
- * Token to username/roles mapping is delegated to an implementation of {@link UsernamePasswordIdentityStore}, which
- * should be registered as CDI bean.
- * 
+ * Token to username/roles mapping is delegated to an implementation of
+ * {@link UsernamePasswordIdentityStore}, which should be registered as CDI
+ * bean.
+ *
  * <p>
- * <b>NOTE:</b> This module makes the simplifying assumption that CDI is available in a SAM. Unfortunately
- * this is not true for every implementation. See https://java.net/jira/browse/JASPIC_SPEC-14
- * 
+ * <b>NOTE:</b> This module makes the simplifying assumption that CDI is
+ * available in a SAM. Unfortunately this is not true for every implementation.
+ * See https://java.net/jira/browse/JASPIC_SPEC-14
+ *
  * @author Arjan Tijms
  *
  */
+@BasicAuthentication
 public class BasicAuthModule extends HttpServerAuthModule {
-	
-	
-	@Override
-	public AuthStatus validateHttpRequest(HttpServletRequest request, HttpServletResponse response, HttpMsgContext httpMsgContext) throws AuthException {
-		
-		String[] credentials = getCredentials(request);
-		if (!isEmpty(credentials)) {
-			
-			UsernamePasswordIdentityStore identityStore = getReferenceOrNull(UsernamePasswordIdentityStore.class);
-			if (identityStore != null) {
-				if (identityStore.authenticate(credentials[0], credentials[1])) {
-					return httpMsgContext.notifyContainerAboutLogin(identityStore.getUserName(), identityStore.getApplicationRoles());
-				}				
-			}			
-		}
-		
-		if (httpMsgContext.isProtected()) {
-			response.setHeader("WWW-Authenticate", "Basic realm=\"test realm:\"");
-			return httpMsgContext.responseUnAuthorized();
-		}
-		
-		return httpMsgContext.doNothing();
-	}
-	
-	private String[] getCredentials(HttpServletRequest request) {
-		
-		String authorizationHeader = request.getHeader("Authorization");
-		if (!isEmpty(authorizationHeader) && authorizationHeader.startsWith("Basic ") ) {
-			return new String(parseBase64Binary(authorizationHeader.substring(6))).split(":");
-		}
-		
-		return null;
-	}
+
+    @Override
+    public AuthStatus validateHttpRequest(HttpServletRequest request, HttpServletResponse response, HttpMsgContext httpMsgContext) throws AuthException {
+
+        String[] credentials = getCredentials(request);
+        if (!isEmpty(credentials)) {
+
+            UsernamePasswordIdentityStore identityStore = getReferenceOrNull(UsernamePasswordIdentityStore.class);
+            if (identityStore != null) {
+                if (identityStore.authenticate(credentials[0], credentials[1])) {
+                    return httpMsgContext.notifyContainerAboutLogin(identityStore.getUserName(), identityStore.getApplicationRoles());
+                }
+            }
+        }
+
+        if (httpMsgContext.isProtected()) {
+            response.setHeader("WWW-Authenticate", "Basic realm=\"test realm:\"");
+            return httpMsgContext.responseUnAuthorized();
+        }
+
+        return httpMsgContext.doNothing();
+    }
+
+    private String[] getCredentials(HttpServletRequest request) {
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if (!isEmpty(authorizationHeader) && authorizationHeader.startsWith("Basic ")) {
+            return new String(parseBase64Binary(authorizationHeader.substring(6))).split(":");
+        }
+
+        return null;
+    }
 
 }
